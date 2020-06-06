ab02aefe28eb0b28e821a4d38952fbf3865cd842
Joshua Webster <darkwolf62@gmail.com>
2020-06-06 02:42:08 -0500

    tinkering with someone else's scrolling animation to get a better understanding.

--- js/app.js
+++ js/app.js
@@ -1,57 +1,11 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+(function() {
+    //scrolling animations
+    const justKeepScrollin = (target, animationDuration) => {
+        const scrollTarget = document.querySelector(target);
+        const scrollTargetLocation =
+            scrollTarget.getBoundingClientRect().top + window.scrollY - 100;
+        const scrollStart = window.pageYOffset;
+        const distance = scrollTargetPosition - startPosition;
+        let startTime = null;
+    }
+})
