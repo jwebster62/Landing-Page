6965f55b0e23e110517a33266f45cfe33f47d7e3
Joshua Webster <darkwolf62@gmail.com>
2020-06-07 14:55:44 -0500

    Creating the Nav Bar
    Trying a few things out to create the navbar.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-(function() {
-    //scrolling animations
-    const justKeepScrollin = (target, animationDuration) => {
-        const scrollTarget = document.querySelector(target);
-        const scrollTargetLocation =
-            scrollTarget.getBoundingClientRect().top + window.scrollY - 100;
-        const scrollStart = window.pageYOffset;
-        const distance = scrollTargetPosition - startPosition;
-        let startTime = null;
-    }
-})
+/*Navigation Global Variable*/
+const allNav = document.getElementById('navbar__list');
+
+/*Section Global Variable*/
+const allSection = document.querySelectorAll('section');
+
+const navCreator = () => {
+
+    let navUI = '';
+    allSection.forEach(section => {
+
+        const sectionID = section.id;
+        const navData = section.dataset.nav;
+
+        navUI += '<li><a class= "menu__link" href="#$[sectionID]">${navData}</a></li>';
+
+    });
+
+    allNav.innerHTML = navUI;
+};
+
+navCreator();
