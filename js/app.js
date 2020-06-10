4365d6aeefe025bc768698775b7cc89a0d8b77ae
Joshua Webster <darkwolf62@gmail.com>
2020-06-09 21:09:09 -0500

    Buttons!
    Created a to-top button.

--- js/app.js
+++ js/app.js
@@ -68,3 +68,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const topButton = document.getElementById("topButton");
+
+//when user scrolls 20px down from top
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+//Go to top on click
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
